88f709f438406f10077db3eaaf42f3b7e568d876
ColemanMatthew-FS <MRColeman1@student.fullsail.edu>
2022-07-08 11:54:55 -0400

    Added pages styling and routes

---
+++ routes/routes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+
+router.get("/", (req,res)=>{
+    console.log(__dirname);
+    res.sendFile(path.join(__dirname, "..", "/views/index.html"));
+});
+
+router.get("/about", (req,res)=>{
+    res.sendFile(path.join(__dirname, "..", "/views/about.html"));
+});
+
+router.get("/index", (req,res)=>{
+    res.sendFile(path.join(__dirname, "..", "/views/index.html"));
+});
+
+router.get("/post", (req,res)=>{
+    res.sendFile(path.join(__dirname, "..", "/views/post.html"));
+});
+module.exports = router;
